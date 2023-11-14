60b019fc1b34083d12584007c0f3261218084432
Tessa Armelim <armelimtessa@gmail.comm>
2023-11-14 19:43:03 +0100

    quete basic 5

--- src/App.jsx
+++ src/App.jsx
@@ -1,29 +1,13 @@
-import { useState } from "react";
+import React from "react";
 import PokemonCard from "./components/PokemonCard";
 import "./App.css";
-import NavBar from "./components/NavBar";
-import { useEffect } from "react";
 
 const pokemonList = [
   {
     name: "bulbasaur",
     imgSrc:
       "https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/official-artwork/1.png",
-  },
-  {
-    name: "charmander",
-    imgSrc:
-      "https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/official-artwork/4.png",
-  },
-  {
-    name: "squirtle",
-    imgSrc:
-      "https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/official-artwork/7.png",
-  },
-  {
-    name: "pikachu",
-    imgSrc:
-      "https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/official-artwork/25.png",
+    type: "Plante",
   },
   {
     name: "mew",
@@ -31,45 +15,16 @@ const pokemonList = [
 ];
 
 function App() {
-  const [pokemonIndex, setPokemonIndex] = useState(0);
-
-  useEffect(() => {
-    alert("Hello Pokemon user :)");
-  }, []);
-
+  const pokemon = pokemonList[0];
   return (
-    <>
-      <PokemonCard pokemon={pokemonList[pokemonIndex]} />
-      <NavBar
-        pokemonIndex={pokemonIndex}
-        setPokemonIndex={setPokemonIndex}
-        pokemonList={pokemonList}
+    <div className="contents">
+      <PokemonCard
+        name={pokemon.name}
+        image={pokemon.imgSrc}
+        type={pokemon.type}
       />
-    </>
-  );
-}
-
-export default App;
-
-/*const pokemonList = [
-  {
-    name: "bulbasaur",
-    imgSrc:
-      "https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/official-artwork/1.png",
-  },
-  {
-    name: "mew",
-  },
-];
-
-function App() {
-  pokemonList;
-  return (
-    <div>
-      <PokemonCard pokemon={pokemonList[0]} />
-      <PokemonCard pokemon={pokemonList[0]} />
     </div>
   );
 }
 
-export default App; */
+export default App;
--- src/components/PokemonCard.jsx
+++ src/components/PokemonCard.jsx
@@ -1,24 +1,25 @@
-const Pokemoncard = (props) => {
-  const { name, imgSrc } = props.pokemon;
-  return (
-    <figure>
-      {imgSrc ? <img src={imgSrc} /> : <p>???</p>}
+import React from "react";
+import PropTypes from "prop-types";
 
-      <figcaption>{name}</figcaption>
+const PokemonCard = (props) => {
+  console.log(props);
+  return (
+    <figure className="pokemon-card">
+      {props.image ? <img src={props.image} alt={props.name} /> : <p>???</p>}
+      <figcaption>
+        <h3>{props.name}</h3>
+        <p>Type : {props.type || "Inconnu"}</p>
+      </figcaption>
     </figure>
   );
 };
-export default Pokemoncard;
 
-/*function PokemonCard(props) {
-  console.log(props);
+PokemonCard.propTypes = {
+  pokemon: PropTypes.shape({
+    name: PropTypes.string.isRequired,
+    type: PropTypes.string.isRequired,
+    image: PropTypes.string,
+  }),
+};
 
-  return (
-    <div>
-      <img src={props.pokemon.imgSrc} />
-      <h1>{props.pokemon.name}</h1>
-    </div>
-  );
-}
-
-export default PokemonCard;*/
+export default PokemonCard;
